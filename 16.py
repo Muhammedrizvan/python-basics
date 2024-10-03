f230c8f198fca3baecbba38c4b9ed84ab1fc058f
91974 <rizvan0326@gmail.com>
2024-10-03 19:08:58 +0530

    04/10/2024

---
+++ 16.py
@@ -0,0 +1,13 @@
+#global variables
+
+#craet a variable outside of a function,and
+#use it inside the function & outside the function
+
+x = "awesome"
+
+def myfunc():
+
+ print("python is"+x)
+
+myfunc()
+print(x)
---
+++ 22.py
@@ -0,0 +1,10 @@
+#check string
+
+#to check if a certain phrase or charecter is present in a string,
+#we can use the keyword in.
+
+
+#check if "free"is present in the following text?
+
+txt = "the best things in life are free!"
+print("free"in txt)
